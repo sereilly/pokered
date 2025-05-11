e96fae10e46553c5f0de874ec60367d148f39d04
Sean Reilly <qwv4851@gmail.com>
2025-05-11 13:38:50 -0700

    Add pewter starters

--- scripts/PewterNidoranHouse.asm
+++ scripts/PewterNidoranHouse.asm
@@ -6,6 +6,9 @@ PewterNidoranHouse_TextPointers:
 	dw_const PewterNidoranHouseNidoranText,       TEXT_PEWTERNIDORANHOUSE_NIDORAN
 	dw_const PewterNidoranHouseLittleBoyText,     TEXT_PEWTERNIDORANHOUSE_LITTLE_BOY
 	dw_const PewterNidoranHouseMiddleAgedManText, TEXT_PEWTERNIDORANHOUSE_MIDDLE_AGED_MAN
+	dw_const PewterNidoranHouseGeodudePokeballText,       TEXT_PEWTERNIDORANHOUSE_GEODUDE_POKE_BALL
+	dw_const PewterNidoranHouseNidoranFPokeballText,         TEXT_PEWTERNIDORANHOUSE_NIDORANF_POKE_BALL
+	dw_const PewterNidoranHouseOddishPokeballText,        TEXT_PEWTERNIDORANHOUSE_ODDISH_POKE_BALL
 
 PewterNidoranHouseNidoranText:
 	text_far _PewterNidoranHouseNidoranText
@@ -21,4 +24,111 @@ PewterNidoranHouseLittleBoyText:
 
 PewterNidoranHouseMiddleAgedManText:
 	text_far _PewterNidoranHouseMiddleAgedManText
+	text_end
+
+; Starter selection
+PewterNidoranHouseGeodudePokeballText:
+	text_asm
+	ld a, STARTER1_PEWTER
+	ld b, PEWTERNIDORANHOUSE_GEODUDE_POKE_BALL
+	jr PewterNidoranHouseSelectedPokeBallScript
+
+PewterNidoranHouseNidoranFPokeballText:
+	text_asm
+	ld a, STARTER2_PEWTER
+	ld b, PEWTERNIDORANHOUSE_NIDORANF_POKE_BALL
+	jr PewterNidoranHouseSelectedPokeBallScript
+
+PewterNidoranHouseOddishPokeballText:
+	text_asm
+	ld a, STARTER3_PEWTER
+	ld b, PEWTERNIDORANHOUSE_ODDISH_POKE_BALL
+	jr PewterNidoranHouseSelectedPokeBallScript
+
+PewterNidoranHouseSelectedPokeBallScript:
+	ld [wCurPartySpecies], a
+	ld [wPokedexNum], a
+	ld a, b
+	ld [wSpriteIndex], a
+	ld hl, wStatusFlags5
+	set BIT_NO_TEXT_DELAY, [hl]
+	predef StarterDex
+	ld hl, wStatusFlags5
+	res BIT_NO_TEXT_DELAY, [hl]
+	call ReloadMapData
+	ld c, 10
+	call DelayFrames
+	ld a, [wSpriteIndex]
+	cp PEWTERNIDORANHOUSE_GEODUDE_POKE_BALL
+	jr z, PewterNidoranHouseYouWantGeodudeText
+	cp PEWTERNIDORANHOUSE_NIDORANF_POKE_BALL
+	jr z, PewterNidoranHouseYouWantNidoranFText
+	jr PewterNidoranHouseYouWantOddishText
+
+PewterNidoranHouseYouWantGeodudeText:
+	ld hl, .Text
+	jr PewterNidoranHouseMonChoiceMenu
+.Text:
+	text_far _PewterNidoranHouseYouWantGeodudeText
+	text_end
+
+PewterNidoranHouseYouWantNidoranFText:
+	ld hl, .Text
+	jr PewterNidoranHouseMonChoiceMenu
+.Text:
+	text_far _PewterNidoranHouseYouWantNidoranFText
+	text_end
+
+PewterNidoranHouseYouWantOddishText:
+	ld hl, .Text
+	jr PewterNidoranHouseMonChoiceMenu
+.Text:
+	text_far _PewterNidoranHouseYouWantOddishText
+	text_end
+
+PewterNidoranHouseMonChoiceMenu:
+	call PrintText
+	ld a, $1
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	call YesNoChoice ; yes/no menu
+	ld a, [wCurrentMenuItem]
+	and a
+	jr nz, PewterNidoranHouseMonChoiceEnd
+	ld a, [wCurPartySpecies]
+	ld [wPlayerStarter], a
+	ld [wNamedObjectIndex], a
+	call GetMonName
+	ld a, $1
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	ld hl, PewterNidoranHouseMonEnergeticText
+	call PrintText
+	xor a ; PLAYER_PARTY_DATA
+	ld [wMonDataLocation], a
+	ld a, 5
+	ld [wCurEnemyLevel], a
+	ld a, [wCurPartySpecies]
+	ld [wPokedexNum], a
+	call AddPartyMon
+	ld hl, wStatusFlags4
+	set BIT_GOT_STARTER, [hl]
+
+	; fade to black and hide the other balls
+	call GBFadeOutToBlack
+	ld a, HS_STARTER_BALL_PEWTER_1
+	ld [wMissableObjectIndex], a
+	predef HideObject
+	ld a, HS_STARTER_BALL_PEWTER_2
+	ld [wMissableObjectIndex], a
+	predef HideObject
+	ld a, HS_STARTER_BALL_PEWTER_3
+	ld [wMissableObjectIndex], a
+	predef HideObject
+	call GBFadeInFromBlack
+
+PewterNidoranHouseMonChoiceEnd:
+	jp TextScriptEnd
+
+PewterNidoranHouseMonEnergeticText:
+	text_far _PewterNidoranHouseMonEnergeticText
+	sound_get_key_item
 	text_end
--- text/PewterNidoranHouse.asm
+++ text/PewterNidoranHouse.asm
@@ -23,3 +23,25 @@ _PewterNidoranHouseMiddleAgedManText::
 	para "If only we had"
 	line "some BADGEs..."
 	done
+
+_PewterNidoranHouseYouWantGeodudeText::
+	text "So! You want the"
+	line "rock #MON,"
+	cont "GEODUDE?"
+	done
+
+_PewterNidoranHouseYouWantNidoranFText::
+	text "So! You want the"
+	line "poison #MON,"
+	cont "NIDORAN?"
+	done
+
+_PewterNidoranHouseYouWantOddishText::
+	text "So! You want the"
+	line "grass #MON,"
+	cont "ODDISH?"
+	done
+
+_PewterNidoranHouseMonEnergeticText::
+	text "Good luck!"
+	prompt
